9e57b7046657dae26acb182c1a97053a893d1a35
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-01-31 01:42:13 +0330

    2023.01.31 - 1

--- app/src/main/kotlin/ir/mahdiparastesh/instatools/Main.kt
+++ app/src/main/kotlin/ir/mahdiparastesh/instatools/Main.kt
@@ -420,6 +420,7 @@ class Main : TriplePageActivity<PageUnf, PageSvd, PageBox>(),
   * Only when you switch to the night mode, the PageSvd overflow menu has the same colour of that theme
   * -
   * Extension:
+  * Count remaining items in the notification of Queuer
   * Live unfollower inspector
   * Conditional jump to bottom
   * Exporter maximum date of top and bottom which would need a calendar picker!?!?
