1ab3a9ea58135a808b8ce9c9a8650962eee33bc4
Mahdi Parastesh <fulcrum1378@gmail.com>
2025-04-16 14:29:55 +0330

    2025.04.16 - 6: some UI fixes, download and like indicators for ListStory

--- android/kotlin/ir/mahdiparastesh/instatools/list/ListStory.kt
+++ android/kotlin/ir/mahdiparastesh/instatools/list/ListStory.kt
@@ -8,7 +8,9 @@ import com.bumptech.glide.signature.ObjectKey
 import ir.mahdiparastesh.instatools.Viewer
 import ir.mahdiparastesh.instatools.api.Story
 import ir.mahdiparastesh.instatools.databinding.ListStoryBinding
+import ir.mahdiparastesh.instatools.util.Utils
 import ir.mahdiparastesh.instatools.view.AnyViewHolder
+import ir.mahdiparastesh.instatools.view.UiTools.vis
 
 class ListStory(private val c: Viewer, var story: Story) :
     RecyclerView.Adapter<AnyViewHolder<ListStoryBinding>>() {
@@ -22,12 +24,29 @@ class ListStory(private val c: Viewer, var story: Story) :
         val med = story.items?.getOrNull(i) ?: return
         h.b.number.text = "${i + 1}"
 
+        // load thumbnail
         Glide.with(c.c)
             .load(med.thumb())
             .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
             .signature(ObjectKey(med.id()))
             .into(h.b.thumb)
 
+        // is media already downloaded?
+        h.b.stored.vis(
+            if (c.c.downloadHistory.isEmpty())
+                false
+            else
+                c.c.downloadHistory.anyStartsWith(
+                    "${story.user.username}_" +
+                        Utils.fileDateTime(Utils.compileSecondsTS(med.taken_at!!)) +
+                        "_${med.id()}"
+                )
+        )
+
+        // is media liked?
+        h.b.liked.vis(med.has_liked == true)
+
+        // clicks
         h.b.click.setOnClickListener {
             c.expandable.expand(
                 story.items?.getOrNull(h.layoutPosition) ?: return@setOnClickListener,
